6ee77263e8f47142fbf288047ec90bb3011514ad
ハン・キョンス <hankyonsu@hankyonsunoMacBook-Air.local>
2023-08-13 17:22:53 +0900

    router変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
 export default {
   target: 'static',
   generate: {subFolders: true},
-  output: {
-    dir: 'mysubfolder/.output',
-    serverDir: 'mysubfolder/.output/server',
-    publicDir: 'mysubfolder/.output/public'
+  router: {
+    base: '/nuxt3-view-test/'
   }
 }
